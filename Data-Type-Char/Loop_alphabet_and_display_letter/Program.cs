cd6cdfb8a0be29776e0f6ad8b831a36ce4d5d1ec
florinfmp <florinfmp@gmail.com>
2023-11-27 13:55:53 +0200

    add data-type char

---
+++ Data-Type-Char/Loop_alphabet_and_display_letter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_alphabet_and_display_letter
+{
+    internal class Program
+    {
+        /*static void Main(string[] args)
+        {
+            for (char myChar = 'a'; myChar <= 'z'; myChar++)
+            {
+                Console.WriteLine(myChar);
+            }
+        }*/
+
+        /*static void Main(string[] args)
+        {
+            for (char myChar = 'A'; myChar <= 'Z'; myChar++)
+            {
+                Console.WriteLine(myChar);
+            }
+        }*/
+
+        //Loop through integers and convert them to letters - letters start at A=65 decimal ...Z=90 decimal; a=97 decimal ... z=122 decimal
+        /*static void Main(string[] args)
+        {
+            for (int myChar = 65; myChar <= 90; myChar++)
+            {
+                Console.WriteLine((char)myChar);
+            }
+        }*/
+
+        static void Main(string[] args)
+        {
+            for (int myChar = 65; myChar <= 90; myChar++)
+            {
+                Console.WriteLine((char)myChar);
+            }
+        }
+    }
+}
---
+++ Data-Type-Char/char/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @char
+{
+    internal class Program
+    {
+        /*static void Main(string[] args)
+        {
+            char myChar = 'A';
+            Console.WriteLine("Character {0} as literal", myChar); //Character A as literal   
+        }*/
+
+        /*static void Main(string[] args)
+        {
+            char myChar1 = '\x0041';
+            Console.WriteLine("Character {0} as hexadecimal", myChar1); //Character A as hexadecimal
+        }*/
+
+        /*static void Main(string[] args)
+        {
+            char myChar2 = (char)65;
+            Console.WriteLine("Character {0} as integer type", myChar2); //Character A as integer type
+        }*/
+
+        static void Main(string[] args)
+        {
+            char myChar3 = '\u0041';
+            Console.WriteLine("Character {0} as unicode", myChar3); //Character A as unicode
+        }
+    }
+}
